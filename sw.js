d5f103e4e34ee4a23993e64111cfa5dc2b0fc7c4
AGUS NUGRAHA <aguspa052@gmail.com>
2026-09-15 01:54:48 +0700

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bpe-cache-v3';
+const CACHE = 'bpe-cache-v1';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
