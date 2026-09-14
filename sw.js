fe99bec658a23a536b1bb4d904324ede77b8ea3a
AGUS NUGRAHA <aguspa052@gmail.com>
2026-09-15 04:38:02 +0700

    Update cache version to 'bpe-cache-v2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bpe-cache-v1';
+const CACHE = 'bpe-cache-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
